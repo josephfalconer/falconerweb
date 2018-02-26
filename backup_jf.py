3ef00216a7b13aae14e8afe8c499f0a85fac8af1
Joe Falconer <joefalconer21@live.co.uk>
2018-02-26 20:23:37 +0000

    Wrote a dictionary of backup objects

--- backup_jf.py
+++ backup_jf.py
@@ -1,5 +1,6 @@
-import os
 import django
+import os
+import re
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "falconerweb.settings")
 django.setup()
@@ -23,6 +24,13 @@ def build_string_from_fields(o):
 		cleaned_name = clean_name(f)
 		string += '{}: {}\n'.format(cleaned_name.upper(), getattr(o, cleaned_name))
 	return '{}\n'.format(string)
+
+def build_dict_from_fields(o):
+	formatted_o = {}
+	for f in o._meta.fields:
+		cleaned_name = clean_name(f)
+		formatted_o[cleaned_name] = getattr(o, cleaned_name)
+	return formatted_o
 
 with open('backup.txt', 'w') as file:
 	file.write('PrimaryRegions:\n\n')
@@ -51,20 +59,38 @@ with open('backup.txt', 'w') as file:
 
 
 # with open('backup-list.py', 'w') as file:
-# 	# file.write('Primary Regions = [\n')
-# 	# for r in PrimaryRegion.objects.all():
-# 	# 	for field in r._meta.fields:
-# 	# 		cleaned_name = clean_name(field)
-# 	# 		string = '{}: {}\n'.format(cleaned_name.upper(), getattr(r, cleaned_name))
-# 	# 		file.write(string)
-# 	# 	file.write('\n')
-# 	# file.write(']')
-# 	list_test = [
-# 		{
-# 			'id': 1
-# 		},
-# 		{
-# 			'id': 2
-# 		}
-# 	]
-# 	file.write(str(list_test))
+# 	backup_dict = {
+# 		'primary_regions': [],
+# 		'sub_regions': [],
+# 		'content_modules': [],
+# 		'skill_areas': [],
+# 		'navigation_links': [],
+# 		'demo_links': []
+# 	}
+
+# 	for o in PrimaryRegion.objects.all():
+# 		formatted_o = build_dict_from_fields(o)
+# 		backup_dict['primary_regions'].append(formatted_o)
+
+# 	for o in SubRegion.objects.all():
+# 		formatted_o = build_dict_from_fields(o)
+# 		backup_dict['sub_regions'].append(formatted_o)
+
+# 	for o in SkillArea.objects.all():
+# 		formatted_o = build_dict_from_fields(o)
+# 		backup_dict['skill_areas'].append(formatted_o)
+
+# 	for o in DemoLink.objects.all():
+# 		formatted_o = build_dict_from_fields(o)
+# 		backup_dict['demo_links'].append(formatted_o)
+
+# 	for o in ContentModule.objects.all():
+# 		formatted_o = build_dict_from_fields(o)
+# 		backup_dict['content_modules'].append(formatted_o)
+
+# 	for o in NavigationLink.objects.all():
+# 		formatted_o = build_dict_from_fields(o)
+# 		backup_dict['navigation_links'].append(formatted_o)	
+
+# 	stringed_dict = str(backup_dict)
+# 	file.write('backup_dict = {}'.format(stringed_dict))
