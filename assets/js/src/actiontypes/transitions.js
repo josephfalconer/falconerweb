7de50aea030c6977345fc30795ab1dfbe7d673c6
Joe Falconer <joefalconer21@live.co.uk>
2018-02-26 20:34:09 +0000

    Started refactoring backend - apps etc

--- assets/js/src/actiontypes/transitions.js
+++
@@ -1,6 +0,0 @@
-export const SET_MOVING_REGIONS = 'transitions/SET_MOVING_REGIONS';
-export const SET_CURRENT_REGION = 'transitions/SET_CURRENT_REGION';
-export const SET_CURRENT_SUB_REGIONS = 'transitions/SET_CURRENT_SUB_REGIONS';
-export const UPDATE_CURRENT_MATCH = 'transitions/UPDATE_CURRENT_MATCH';
-export const SET_TEXT_COLOUR = 'transitions/SET_TEXT_COLOUR';
-export const SET_OUTGOING_REGION = 'transitions/SET_OUTGOING_REGION';
