d22af808e6920f58b7b24f3c6c771e747d76f085
Joe Falconer <joefalconer21@live.co.uk>
2018-02-27 22:25:12 +0000

    Little tweak/tidy up of Main

--- static/js/src/components/Main.js
+++ static/js/src/components/Main.js
@@ -4,81 +4,76 @@ import { withRouter } from 'react-router';
 import { Link, Route } from 'react-router-dom';
 
 import IncomingRegion from '../components/IncomingRegion';
-// import DirectionButtons from '../components/DirectionButtons';
-// import Navigation from '../components/Navigation';
 import OutgoingRegion from '../components/OutgoingRegion';
 
 
-class Regions extends Component {
-	static propTypes = {
-        primaryRegions: PropTypes.array,
-        subRegions: PropTypes.array,
-        isMovingRegions: PropTypes.bool.isRequired,
-        outgoingRegion: PropTypes.object,
-        regionTextColour: PropTypes.string.isRequired
+function renderChildZones(parentPath, childZones) {
+	if (childZones) {
+		return childZones.map(childZone => (
+			<Route 
+				key={`region-${childZone.path_hash}`}
+				path={`/${parentPath}/${childZone.path_hash}/`} 
+				render={() => (
+					<IncomingRegion 
+						pathToParent={parentPath}
+						data={childZone} 
+					/>
+				)} 
+			/>
+		))
 	}
+	return null;
+}
 
-	renderChildZones = (parentPath, childZones) => {
-		if (childZones) {
-			return childZones.map(childZone => (
-				<Route 
-					key={`region-${childZone.path_hash}`}
-					path={`/${parentPath}/${childZone.path_hash}/`} 
-					render={() => (
-						<IncomingRegion 
-							pathToParent={parentPath}
-							data={childZone} 
+const Main = props => {
+	const { parentZones, outgoingRegion, isMovingRegions } = props;
+	if (isMovingRegions) {
+		// TODO - replace with ref?
+		document.getElementById('regions').scrollTop = 0;
+	}
+	return (
+		<main id="regions" className="regions">	
+			{outgoingRegion && isMovingRegions &&
+				<OutgoingRegion data={outgoingRegion} />
+			}
+			{parentZones && parentZones.map(parentZone => {
+				const { path_hash, child_zones } = parentZone;
+				return (
+					<div key={`region-${path_hash}`}>
+						<Route
+							exact
+							path={`/${path_hash}/`} 
+							render={() => (
+								<IncomingRegion 
+									pathToParent={path_hash}
+									data={parentZone} 
+									ownChildRegions={child_zones}
+								/>
+							)}
 						/>
-					)} 
-				/>
-			))
-		}
-		return null;
-	}
+						{renderChildZones(path_hash, child_zones)}
+					</div>
+				)
+			})}
+		</main>
+	)
+}
 
-	render() {
-		const { parentZones, outgoingRegion, isMovingRegions } = this.props;
-		if (isMovingRegions) {
-			// TODO - replace with ref?
-			document.getElementById('regions').scrollTop = 0;
-		}
-		return (
-			<main id="regions" className="regions">	
-				{outgoingRegion && isMovingRegions &&
-					<OutgoingRegion data={outgoingRegion} />
-				}
-				{parentZones && parentZones.map(parentZone => {
-					const { path_hash, child_zones } = parentZone;
-					return (
-						<div key={`region-${path_hash}`}>
-							<Route
-								exact
-								path={`/${path_hash}/`} 
-								render={() => (
-									<IncomingRegion 
-										pathToParent={path_hash}
-										data={parentZone} 
-										ownChildRegions={child_zones}
-									/>
-								)}
-							/>
-							{this.renderChildZones(path_hash, child_zones)}
-						</div>
-					)
-				})}
-			</main>
-		)
-	};
+Main.propTypes = {
+    primaryRegions: PropTypes.array,
+    subRegions: PropTypes.array,
+    isMovingRegions: PropTypes.bool.isRequired,
+    outgoingRegion: PropTypes.object,
+    regionTextColour: PropTypes.string.isRequired
 }
 
 const mapStateToProps = state => (
     {
-        parentZones: state.data.primaryRegions,
-        subRegions: state.data.subRegions,
+        parentZones: state.data.parentZones,
         isMovingRegions: state.transitions.isMovingRegions,
         outgoingRegion: state.transitions.outgoingRegion,
         regionTextColour: state.transitions.currentTextColour,
     }
 );
 
-export default withRouter(connect(mapStateToProps)(Regions));
+export default withRouter(connect(mapStateToProps)(Main));
