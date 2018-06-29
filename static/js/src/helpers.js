2fa227812da12eca9e2160a9bf6d50177a19e581
Joe Falconer <joefalconer21@live.co.uk>
2018-06-30 00:46:51 +0100

    Helpers - switched out consts with anonymous functions for named functions

--- static/js/src/helpers.js
+++ static/js/src/helpers.js
@@ -1,20 +1,20 @@
-export const isSideways = (currentZone, outgoingZone) => {
+export function isSideways(currentZone, outgoingZone) {
 	return outgoingZone.y == currentZone.y && Math.abs(outgoingZone.x - currentZone.x) == 1;
 }
 
-export const isVertical = (currentZone, outgoingZone) => {
+export function isVertical(currentZone, outgoingZone) {
 	return outgoingZone.x == currentZone.x && Math.abs(outgoingZone.y - currentZone.y) == 1;
 }
 
-export const isLeftwards = (currentZone, outgoingZone) => {
+export function isLeftwards(currentZone, outgoingZone) {
 	return currentZone.x < outgoingZone.x;
 }
 
-export const isUpwards = (currentZone, outgoingZone) => {
+export function isUpwards(currentZone, outgoingZone) {
 	return currentZone.y < outgoingZone.y;
 }
 
-export const formatHash = (newHash, parentPathHash) => {
-	let isToParent = newHash === parentPathHash;
-	return `/${parentPathHash}${isToParent ? '' : '/' + newHash}`;
+export function formatVerticalPath(parentPath, targetPath) {
+	const isTopTarget = targetPath === parentPath;
+	return `/${parentPath}${isTopTarget ? '' : '/' + targetPath}`;
 }
