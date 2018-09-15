6731b922a8f9ffa675ba33e355cac2a32cca5675
Joe Falconer <joefalconer21@live.co.uk>
2018-09-15 14:16:42 +0100

    Use ref instead of getELementByID to reset scrollTop and render child_zones without helper function

--- static/js/src/components/Main.js
+++ static/js/src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { Link, Route } from 'react-router-dom';
@@ -6,59 +6,60 @@ import { Link, Route } from 'react-router-dom';
 import IncomingZone from '../components/IncomingZone';
 import OutgoingZone from '../components/OutgoingZone';
 
-
-function renderChildZones(parentPath, childZones) {
-	if (childZones) {
-		return childZones.map(childZone => (
-			<Route
-				key={`zone-${childZone.path_hash}`}
-				path={`/${parentPath}/${childZone.path_hash}/`}
-				render={() => (
-					<IncomingZone
-						pathToParent={parentPath}
-						data={childZone}
-					/>
-				)}
-			/>
-		))
+class Main extends PureComponent {
+	constructor(props) {
+		super(props)
+		this.setContainerRef = element => this.containerElement = element;
 	}
-	return null;
-}
 
-const Main = props => {
-	const { parentZones, outgoingZone, isMovingZones, location } = props;
-	if (isMovingZones) {
-		// TODO - replace with ref?
-		document.getElementById('regions').scrollTop = 0;
+	componentWillReceiveProps(nextProps) {
+		if (this.containerElement && nextProps.isMovingZones) {
+			this.containerElement.scrollTop;
+		}
 	}
-	return (
-		<main id="regions" className="regions">
-			{outgoingZone && isMovingZones &&
-				<OutgoingZone data={outgoingZone} />
-			}
-			{parentZones && parentZones.map(parentZone => {
-				const { path_hash, child_zones } = parentZone;
-				return (
-					<div key={`zone-${path_hash}`}>
-						<Route
-							exact
-							path={`/${path_hash}/`}
-							render={() => (
-								<IncomingZone
-									pathToParent={path_hash}
-									data={parentZone}
-									isParentZone
-									ownChildZones={child_zones}
-									location={location}
+
+	render() {
+		const { parentZones, outgoingZone, isMovingZones, location } = this.props;
+		return (
+			<main ref={this.setContainerRef} className="regions">
+				{outgoingZone && isMovingZones &&
+					<OutgoingZone data={outgoingZone} />
+				}
+				{parentZones && parentZones.map(parentZone => {
+					const { path_hash, child_zones } = parentZone;
+					return (
+						<div key={`zone-${path_hash}`}>
+							<Route
+								exact
+								path={`/${path_hash}/`}
+								render={() => (
+									<IncomingZone
+										pathToParent={path_hash}
+										data={parentZone}
+										isParentZone
+										ownChildZones={child_zones}
+										location={location}
+									/>
+								)}
+							/>
+							{child_zones && child_zones.map(childZone => (
+								<Route
+									key={`zone-${childZone.path_hash}`}
+									path={`/${path_hash}/${childZone.path_hash}/`}
+									render={() => (
+										<IncomingZone
+											pathToParent={path_hash}
+											data={childZone}
+										/>
+									)}
 								/>
-							)}
-						/>
-						{renderChildZones(path_hash, child_zones)}
-					</div>
-				)
-			})}
-		</main>
-	)
+							))}
+						</div>
+					)
+				})}
+			</main>
+		);		
+	}
 }
 
 Main.propTypes = {
