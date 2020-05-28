9b88357c0feeffb87968e279d6688531e927e0d0
Joe Falconer <joseph@dabapps.com>
2020-05-29 00:33:51 +0100

    Update styles for tools accordion and do some clean up

--- static/js/src/components/modules/ToolsAccordion.js
+++ static/js/src/components/modules/ToolsAccordion.js
@@ -3,7 +3,18 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { updateStoreState } from '../../actions';
 
+const getContentRefName = index => `accordion-content-${index}`;
+
 class ToolsAccordion extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.setAccordionContent = element => {
+      if (element) {
+        this[getContentRefName(element.getAttribute('index'))] = element;
+      }
+    }
+  }
+
   componentDidMount() {
     if (!this.props.tools.length) {
       fetch('/api/toolkit/')
@@ -12,62 +23,69 @@ class ToolsAccordion extends PureComponent {
     }
   }
 
-  openPanel = (e, index) => {
-    e.preventDefault();
-    const contentBoxes = document.getElementsByClassName('accordion__contentinner');
-    const targetPanelHeight = contentBoxes[index].offsetHeight / 16;
-    // if current panel was clicked close the current panel
-    this.props.updateStoreState({
-      currentAccordionIndex: index == this.props.currentAccordionIndex ? -1 : index,
-      currentAccordionHeight: `${targetPanelHeight}rem`
-    });
-  }
-
   render() {
     return (
       <div>
-        <div className="accordion text">
-          {this.props.tools.map((tool, index) => {
-            const isCurrentPanel = index == this.props.currentAccordionIndex;
-            const panelClass = isCurrentPanel ? 'accordion__panel accordion__panel--current' : 'accordion__panel';
-            const heightStyle = {
-              height: isCurrentPanel ? this.props.currentAccordionHeight : '6.25rem'
-            };
-            return (
-              <article className={panelClass} key={`accordion-panel-${index}`}>
-                <div className="accordion__heading">
-                  <a className="accordion__trigger" href="" onClick={e => { this.openPanel(e, index); }}>
-                    <div className="column">
-                      <h3 className="accordion__title">{tool.title}</h3>
-                      <span className="accordion__tooltip accordion__read">Read</span>
-                      <span className="accordion__tooltip accordion__close">Close</span>
-                    </div>
+        {this.props.tools.length ? (
+          <div className="accordion text">
+            {this.props.tools.map((tool, index) => {
+              const isCurrentSection = index == this.props.currentAccordionSection;
+              return (
+                <article key={`accordion-section-${index}`} className="accordion__section">
+                  <a className="accordion__heading" href="">
+                    <h3
+                      index={index}
+                      onClick={this.openSection}
+                      className="accordion__title"
+                    >
+                      {tool.title}
+                    </h3>
+                    <span className="accordion__tooltip">
+                      {isCurrentSection ? 'Close' : 'Open'}
+                    </span>
                   </a>
-                </div>
-                <div className="accordion__content" style={heightStyle}>
-                  <div className="accordion__contentinner" dangerouslySetInnerHTML={{__html: tool.text}}></div>
-                </div>
-              </article>
-            );
-          })}
-        </div>
-        <p className="page__intro">
-          Click
-          {' '}
-          <Link to="/demos/play">here</Link>
-          {' '}
-          to play some JavaScript games I made.
-        </p>
+                  <div className="accordion__content" style={this.getHeightStyle(isCurrentSection)}>
+                    <div
+                      ref={this.setAccordionContent}
+                      index={index}
+                      className="accordion__contentinner"
+                      dangerouslySetInnerHTML={{__html: tool.text}}
+                    ></div>
+                  </div>
+                </article>
+              );
+            })}
+          </div>
+        ) : 'Loading...'}
       </div>
     );
   }
+
+  getHeightStyle = isCurrentSection => ({
+    height: isCurrentSection ? this.props.currentAccordionSectionHeight : '0'
+  });
+
+  openSection = event => {
+    event.preventDefault();
+    const index = event.target.getAttribute('index');
+    const contentHeight = this[getContentRefName(index)].offsetHeight;
+    // Close sec if the current section was clicked
+    this.props.updateStoreState({
+      currentAccordionSection: index == this.props.currentAccordionSection ? -1 : index,
+      currentAccordionSectionHeight: `${contentHeight / 16}rem`
+    });
+  }
 }
 
-const mapStateToProps = ({tools, currentAccordionIndex, currentAccordionHeight}) => (
+const mapStateToProps = ({
+  tools,
+  currentAccordionSection,
+  currentAccordionSectionHeight
+}) => (
   {
     tools: tools || [],
-    currentAccordionIndex,
-    currentAccordionHeight
+    currentAccordionSection,
+    currentAccordionSectionHeight
   }
 );
 
